b83b610e64db9aa266923273f8420e765d6ccd74
guimroque <guilhermemr_2017@hotmail.com>
2024-04-04 23:01:20 -0300

    feat: plot infered blocks on simulator

---
+++ classes/Objects/object.py
@@ -0,0 +1,40 @@
+import random
+from robodk import robolink, robomath
+from CONSTANTES import REFERENCES, BLOCKS, BLOCKS_COLOR, BLOCKS_POSITION
+
+
+class Objects:
+    def __init__(self):
+        self.rdk = robolink.Robolink()
+        self.reference_frame = self.rdk.Item(REFERENCES['CENTER_TABLE'], robolink.ITEM_TYPE_FRAME)
+
+    def insert(self, coords=[0, 0], color="BLACK"): #todo: verify why recover a name of the class and use correct color
+        #params to new item
+        name = BLOCKS[color] + str(random.randint(0, 9999))
+        _color = BLOCKS_COLOR[color]
+        position = robomath.PosePP(# items are generated just on table
+            coords[0], # x_pos
+            coords[1], # y_pos,
+            BLOCKS_POSITION['TABLE']['z_pos'],
+            BLOCKS_POSITION['TABLE']['x_rot'],
+            BLOCKS_POSITION['TABLE']['y_rot'],
+            BLOCKS_POSITION['TABLE']['z_rot']
+        )
+
+        #get exists item to copy
+        item = self.rdk.Item(REFERENCES['BLOCK_BLACK'], robolink.ITEM_TYPE_OBJECT)
+        if not item.Valid():
+            raise Exception('Item not valid')
+        item.Copy()
+
+        self.rdk.Render(False)
+
+        #create a new item
+        new_item = self.reference_frame.Paste()
+        new_item.setName(name)
+        new_item.setPose(position)
+        new_item.setVisible(True, False)
+        new_item.Recolor(_color)
+
+        item.setVisible(False, False)
+        self.rdk.Render(True)
--- infer.py
+++ infer.py
@@ -1,10 +1,18 @@
-from ultralytics import YOLO
-from classes.frame.frame import Frame, IMAGE, MODEL_BLOCKS, MODEL_TABLE
-import cv2
+from classes.Frame.frame import Frame, IMAGE, MODEL_BLOCKS, MODEL_TABLE
+from CONSTANTES import LOGS
+from classes.Objects.object import Objects
 
-# Infer
+
+# Infer // todo: fix this infers, move constantes
 table_frames = Frame.infer_frames(IMAGE, MODEL_TABLE, 'table')
 blocks_frames = Frame.infer_frames(IMAGE, MODEL_BLOCKS, 'blocks')
 reason_pixels_mm = Frame.get_proportionality()
 
 Frame.draw_frames(blocks_frames, table_frames[0], True, IMAGE, 'name.png', reason_pixels_mm['average'])
+
+#plot items on robodk
+for frame in blocks_frames:
+    coords = Frame.get_coords(table_frames[0], frame, reason_pixels_mm['average'])
+    print(f"{LOGS['CONNECT']} {coords}")
+    Objects().insert(coords=[coords['x_mm'], coords['y_mm']])
+
